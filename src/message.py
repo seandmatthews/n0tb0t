62cbfde6acbb71c590240a90967ddfd8c5481ceb
Sean <matthews.sean.d@gmail.com>
2017-04-01 20:21:20 -0500

    fixing message

--- src/message.py
+++ src/message.py
@@ -1,16 +1,17 @@
 class Message:
-	'''
-	So what does a message have?
-	a service (twitch, beam, whatever)
-	a message type (whisper, public message, direct message, etc?)
-	a user (who sent the message)
-	message content = "!ogod buzzer baby"
-
-	The goal is for the bot to act_on(Message)
-	And for the but to push down sockets varous Messages
-	This should just hold data...
-	'''
-	service = DEFAULT_SERVICE
-	message_type = DEFAULT_MESSAGE_TYPE
-	user = DEFAULT_USER
-	content = DEFAULT_CONTENT
+    '''
+    So what does a message have?
+    a service (twitch, beam, whatever)
+    a message type (whisper, public message, direct message, etc?)
+    a user (who sent the message)
+    message content = "!ogod buzzer baby"
+    
+    The goal is for the bot to act_on(Message)
+    And for the but to push down sockets varous Messages
+    This should just hold data...
+    '''
+    def __init__(self, service=None, message_type=None, user=None, content=None):
+        self.service = service
+        self.message_type = message_type
+        self.user = user
+        self.content = content
