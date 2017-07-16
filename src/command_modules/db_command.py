1cf157d8afe07a422af918482b62072165a17c20
Aaron Thomas <1d20m6@gmail.com>
2017-07-16 11:30:44 -0400

    Added skeleton command for adding database commands to db_command module

--- src/command_modules/db_command.py
+++ src/command_modules/db_command.py
@@ -8,3 +8,15 @@ class DatabaseCommand(Command):
     def _execute(self, message, service):
         message.contents = self.echo_text
         service.add_to_message_queue(message)
+
+
+class AddDatabaseCommand(Command)
+    def __init__(self, allowed_privileges, disallowed_privileges, chat_command):
+        super().__init__(allowed_privileges, disallowed_privileges, chat_command)
+
+    def _execute(self, message, service):
+        '''
+        @todo(metruption):
+        write preconditions and postconditions for this function
+        '''
+        pass
