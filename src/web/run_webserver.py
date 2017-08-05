e71b1f220c1df6ec156f2226fa7490467d28e2fc
Sean <matthews.sean.d@gmail.com>
2017-08-05 09:24:02 -0500

    Fixed webserver path nonsense

--- src/web/run_webserver.py
+++ src/web/run_webserver.py
@@ -1,3 +1,12 @@
+from inspect import getsourcefile
+import os
+import sys
+
+current_path = os.path.abspath(getsourcefile(lambda: 0))
+current_dir = os.path.dirname(current_path)
+grandparent_dir = os.path.join(current_dir, os.pardir, os.pardir)
+sys.path.insert(0, grandparent_dir)
+
 from src.web.flask_webserver import app
 
 if __name__ == '__main__':
